841a809497ef26a8ed32c08f8dd5f0453729722c
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2023-10-03 13:54:58 +0100

    Adding existing code. This seems to work well

---
+++ RecordAnalyser/NodeHelpers.cs
@@ -0,0 +1,206 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
+// https://learn.microsoft.com/en-us/dotnet/api/microsoft.codeanalysis.itypesymbol?view=roslyn-dotnet-4.6.0
+
+namespace JS_RecordAnalyser;
+
+internal enum ValueEqualityResult
+{
+	Ok,
+	Failed,
+	NestedFailed
+}
+
+internal static class NodeHelpers
+{
+	/// <summary>
+	/// Wrapper around the various equality checks
+	/// </summary>
+	internal static (ValueEqualityResult, string? membername) ValueEqualityWrapper(ITypeSymbol? type)
+	{
+		type = NodeHelpers.GetUnderlyingType(type); // unwrap any nullable
+		if (type == null) return (ValueEqualityResult.Ok, null);
+
+		if (IsStrictlyInvalid(type)) return (ValueEqualityResult.Failed, null);      // object and dynamic
+
+		if (HasSimpleEquality(type)) return (ValueEqualityResult.Ok, null);       // primitive types, string, enum
+		if (!type.IsTupleType)
+		{
+			// for tuples we ignore Equals(T) and Equals(object)
+			if (HasEqualsTMethod(type)) return (ValueEqualityResult.Ok, null);        // Equals(T) not inherited
+			if (HasEqualsObjectMethod(type)) return (ValueEqualityResult.Ok, null);   // Equals(object) overridden and not inherited
+
+			if (IsRecordType(type)) return (ValueEqualityResult.Ok, null);            // a record is ok
+			if (IsClassType(type)) return (ValueEqualityResult.Failed, null);         // a class is not ok
+		}
+
+		// get the members of the tuple or struct
+		IEnumerable<ISymbol>? members = null;
+		if (type is INamedTypeSymbol namedTypeSymbol && namedTypeSymbol.IsTupleType)
+			members = namedTypeSymbol.TupleElements;
+		else if (IsStruct(type))
+			members = type.GetMembers();
+
+		if (members != null)
+		{
+			// this compound type has members. Check each one
+			foreach (var member in members)
+			{
+				var memberType = member switch
+				{
+					IPropertySymbol property => property.Type,
+					IFieldSymbol field => field.Type,
+					_ => null,
+				};
+
+				if (memberType == null) continue;
+				var (result, _) = ValueEqualityWrapper(memberType);
+
+				if (result != ValueEqualityResult.Ok)
+				{
+					// if the nested type fails, return the type name
+					var nestedtypefail = memberType?.ToDisplayString(NullableFlowState.None) ?? "UNKNOWN";
+					return (ValueEqualityResult.NestedFailed, nestedtypefail);
+				}
+			}
+
+			return (ValueEqualityResult.Ok, null); // compound type passes equality test
+		}
+
+		// by default, fail
+		return (ValueEqualityResult.Failed, null);
+	}
+
+	/// <summary>
+	/// Does this record have an Equals(T) method?
+	/// </summary>
+	internal static bool RecordHasEquals(SyntaxNodeAnalysisContext context)
+	{
+		var recordDeclaration = (RecordDeclarationSyntax)context.Node;
+		var recordTypeSymbol = context.SemanticModel.GetDeclaredSymbol(recordDeclaration);
+
+		foreach (var member in recordDeclaration.Members)
+		{
+			var memberSymbol = context.SemanticModel.GetDeclaredSymbol(member);
+
+			if (memberSymbol is IMethodSymbol methodSymbol)
+			{
+				// this is a method member. Check if its Equals(T), and if so no further checks are needed
+				if (methodSymbol.Name == "Equals"
+					&& methodSymbol.ReturnType.SpecialType == SpecialType.System_Boolean
+					&& methodSymbol.Parameters.Length == 1
+					&& methodSymbol.Parameters[0].Type.Equals(recordTypeSymbol, SymbolEqualityComparer.Default)) return true;
+			}
+		}
+
+		return false;
+	}
+
+	/// <summary>
+	/// Get the type and name of the property or field
+	/// </summary>
+	internal static (ITypeSymbol? membertype, string? membername, bool isproperty) GetPropertyOrFieldUnderlyingType(SyntaxNodeAnalysisContext context, MemberDeclarationSyntax member)
+	{
+		// get the field / property type and name
+		ITypeSymbol? type;
+		string? memberstr;
+		bool isproperty;
+		if (member is PropertyDeclarationSyntax propertyDeclaration)
+		{
+			type = context.SemanticModel.GetTypeInfo(propertyDeclaration.Type).Type;
+			memberstr = propertyDeclaration.Identifier.ValueText;
+			isproperty = true;
+		}
+		else if (member is FieldDeclarationSyntax fieldDeclaration)
+		{
+			type = context.SemanticModel.GetTypeInfo(fieldDeclaration.Declaration.Type).Type;
+			memberstr = fieldDeclaration.Declaration.Variables[0].Identifier.ValueText;
+			isproperty = false;
+		}
+		else
+			return (null, null, false);
+
+		// get the type of the member, and unwrap it if it's nullable
+		return (NodeHelpers.GetUnderlyingType(type), memberstr, isproperty);
+	}
+
+	/// <summary>
+	/// Given Nullable type, return the underlying type. If not nullable, return the type itself.
+	/// </summary>
+	internal static ITypeSymbol? GetUnderlyingType(ITypeSymbol? type)
+	{
+		if (type == null) return null;
+		if (!IsNullableValueType(type)) return type;
+
+		var namedtype = type as INamedTypeSymbol;
+		return namedtype?.TypeArguments[0];
+	}
+
+	private static bool IsNullableValueType(ITypeSymbol? type) => type?.IsValueType == true && type.NullableAnnotation == NullableAnnotation.Annotated;
+
+	private static bool IsClassType(ITypeSymbol? type) => type?.TypeKind == TypeKind.Class;
+
+	private static bool IsObjectType(ITypeSymbol? type) => type?.SpecialType == SpecialType.System_Object;
+
+	/// <summary>
+	/// Record class or Record struct. *** CONFIRM THIS BEHAVIOUR
+	/// </summary>
+	private static bool IsRecordType(ITypeSymbol? type) => type != null && (type.IsRecord || (type.TypeKind == TypeKind.Struct && type.IsReadOnly));
+
+	/// <summary>
+	/// True if this is a struct
+	/// </summary>
+	private static bool IsStruct(ITypeSymbol? type) => type != null && type.TypeKind == TypeKind.Struct;
+
+	/// <summary>
+	/// Is this always invalid? (object, dynamic, etc)
+	/// </summary>
+	private static bool IsStrictlyInvalid(ITypeSymbol? type) =>
+		type?.SpecialType == SpecialType.System_Object || type?.TypeKind == TypeKind.Dynamic;
+
+	/// <summary>
+	/// Simple equality means a primitive type, string or enum
+	/// </summary>
+	private static bool HasSimpleEquality(ITypeSymbol? type) => type != null && (type.TypeKind == TypeKind.Enum || IsPrimitiveType(type));
+
+	/// <summary>
+	/// Is this a primitive type? Includes string
+	/// </summary>
+	private static bool IsPrimitiveType(ITypeSymbol? type)
+	{
+		return type != null
+			&& type.SpecialType switch
+			{
+				SpecialType.System_Boolean or SpecialType.System_SByte or SpecialType.System_Int16 or SpecialType.System_Int32 or SpecialType.System_Int64 or SpecialType.System_Byte or SpecialType.System_UInt16 or SpecialType.System_UInt32 or SpecialType.System_UInt64 or SpecialType.System_Single or SpecialType.System_Double or SpecialType.System_Char or SpecialType.System_String
+				=> true,
+				_ => false,
+			};
+	}
+
+	/// <summary>
+	/// Does this type have an Equals(T) method that takes a single parameter of the same type?
+	/// </summary>
+	private static bool HasEqualsTMethod(ITypeSymbol? type) =>
+		type?.GetMembers("Equals")
+			.OfType<IMethodSymbol>()
+			.Any(m => m.Parameters.Length == 1
+				&& m.Parameters[0].Type.Equals(type, SymbolEqualityComparer.Default)
+				&& !m.IsStatic
+				&& !m.IsOverride) == true;
+
+	/// <summary>
+	/// Does this type have an Equals(object) override method defined in this type?
+	/// </summary>
+	private static bool HasEqualsObjectMethod(ITypeSymbol? type) =>
+		type?.GetMembers("Equals")
+			.OfType<IMethodSymbol>()
+			.Any(m => m.Parameters.Length == 1
+				&& IsObjectType(m.Parameters[0].Type)
+				&& !m.IsStatic
+				&& m.IsOverride
+				&& m.ContainingType.Equals(type, SymbolEqualityComparer.Default)) == true;
+}
---
+++ RecordAnalyser/RecordAnalyser.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace JS_RecordAnalyser;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class RecordAnalyzer : DiagnosticAnalyzer
+{
+	private static readonly DiagnosticDescriptor ParamValueSemanticsRule = new("JSV01", "Parameter semantics warning",
+		"Member '{0}' does not have value semantics", "Design", DiagnosticSeverity.Warning, isEnabledByDefault: true,
+		description: "Member '{0}' does not have value semantics.");
+	private static readonly DiagnosticDescriptor PropertyValueSemanticsRule = new("JSV02", "Property semantics warning",
+		"Property '{0}' does not have value semantics", "Design", DiagnosticSeverity.Warning, isEnabledByDefault: true,
+		description: "Property '{0}' does not have value semantics.");
+	private static readonly DiagnosticDescriptor FieldValueSemanticsRule = new("JSV03", "Field semantics warning",
+		"Field '{0}' does not have value semantics", "Design", DiagnosticSeverity.Warning, isEnabledByDefault: true,
+		description: "Field '{0}' does not have value semantics.");
+	private static readonly DiagnosticDescriptor NestedValueSemanticsRule = new("JSV04", "Nested semantics warning",
+		"Nested '{0}' does not have value semantics", "Design", DiagnosticSeverity.Warning, isEnabledByDefault: true,
+		description: "Nested '{0}' does not have value semantics.");
+
+	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(ParamValueSemanticsRule, PropertyValueSemanticsRule,
+		FieldValueSemanticsRule, NestedValueSemanticsRule);
+
+	public override void Initialize(AnalysisContext context)
+	{
+		context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+		context.EnableConcurrentExecution();
+
+		context.RegisterSyntaxNodeAction(AnalyzeRecordDeclaration, SyntaxKind.RecordDeclaration);
+		context.RegisterSyntaxNodeAction(AnalyzeRecordDeclaration, SyntaxKind.RecordStructDeclaration);
+	}
+
+	private static void AnalyzeRecordDeclaration(SyntaxNodeAnalysisContext context)
+	{
+		var recordDeclaration = (RecordDeclarationSyntax)context.Node;
+		//var recordTypeSymbol = context.SemanticModel.GetDeclaredSymbol(recordDeclaration);
+
+		// if the record has an Equals(T) method, then we're ok. No need to check further
+		if (NodeHelpers.RecordHasEquals(context)) return;
+
+		// check the parameter list eg record A(int i, int j)
+		var recordparams = recordDeclaration.ParameterList?.Parameters;
+		if (recordparams != null)
+			foreach (var recparam in recordparams)
+			{
+				// get the type of the member, and unwrap it if it's nullable
+				var parameterSymbol = context.SemanticModel.GetDeclaredSymbol(recparam);
+				var type = parameterSymbol?.Type;
+				if (type == null) continue;
+
+				// if the property has value semantics, then we're ok
+				var (result, errormember) = NodeHelpers.ValueEqualityWrapper(type);
+				if (result == ValueEqualityResult.Ok) continue;
+
+				// otherwise, we have a problem. show a diagnostic
+				var typestr = type?.ToDisplayString(NullableFlowState.None) ?? "";
+				var memberstr = recparam.Identifier.ValueText;
+				var rule = (result == ValueEqualityResult.NestedFailed) ? NestedValueSemanticsRule : ParamValueSemanticsRule;
+				var args = errormember == null ? $"{typestr} {memberstr}" : $"{typestr} {memberstr} ({errormember})";
+
+				var diagnostic = Diagnostic.Create(rule, recparam.GetLocation(), args);
+				context.ReportDiagnostic(diagnostic);
+			}
+
+		// check fields and properties
+		foreach (var member in recordDeclaration.Members)
+		{
+			var (unwrappedtype, memberstr, isproperty) = NodeHelpers.GetPropertyOrFieldUnderlyingType(context, member);
+			if (unwrappedtype == null) continue;
+
+			// if the property has value semantics, then we're ok
+			var (result, errormember) = NodeHelpers.ValueEqualityWrapper(unwrappedtype);
+			if (result == ValueEqualityResult.Ok) continue;
+
+			// otherwise, we have a problem. show a diagnostic
+			var rule = GetRule(result, isproperty);
+			var typestr = unwrappedtype?.ToDisplayString(NullableFlowState.None) ?? "";
+			memberstr ??= "?";
+			var args = errormember == null ? $"{typestr} {memberstr}" : $"{typestr} {memberstr} ({errormember})";
+
+			var diagnostic = Diagnostic.Create(rule, member.GetLocation(), args);
+			context.ReportDiagnostic(diagnostic);
+		}
+	}
+
+	private static DiagnosticDescriptor GetRule(ValueEqualityResult result, bool isproperty)
+	{
+		if (result == ValueEqualityResult.NestedFailed) return NestedValueSemanticsRule;
+		return isproperty ? PropertyValueSemanticsRule : FieldValueSemanticsRule;
+	}
+}
