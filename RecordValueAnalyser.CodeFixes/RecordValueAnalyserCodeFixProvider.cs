6f14a7399aa5c5453db38fa2940193e08cf165ef
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2023-10-18 15:23:28 +0100

    Ambient namespace, and code tidyers

--- RecordValueAnalyser.CodeFixes/RecordValueAnalyserCodeFixProvider.cs
+++ RecordValueAnalyser.CodeFixes/RecordValueAnalyserCodeFixProvider.cs
@@ -10,82 +10,95 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-// #pragma warning disable IDE0079 // Remove unnecessary suppression
-// #pragma warning disable CS0618 // Type or member is obsolete
-// #pragma warning disable CS0162 // Unreachable code detected
+namespace RecordValueAnalyser;
 
-namespace RecordValueAnalyser
+[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(RecordValueAnalyserCodeFixProvider)), Shared]
+public class RecordValueAnalyserCodeFixProvider : CodeFixProvider
 {
-	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(RecordValueAnalyserCodeFixProvider)), Shared]
-	public class RecordValueAnalyserCodeFixProvider : CodeFixProvider
+	public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(RecordValueAnalyser.DiagnosticId);
+
+	public sealed override FixAllProvider GetFixAllProvider() =>
+		// See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+		WellKnownFixAllProviders.BatchFixer;
+
+	/// <summary>
+	/// Register code fix for record classes and record structs
+	/// </summary>
+	public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
 	{
-		public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(RecordValueAnalyser.DiagnosticId);
+		var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-		public sealed override FixAllProvider GetFixAllProvider() =>
-			// See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
-			WellKnownFixAllProviders.BatchFixer;
+		var diagnostic = context.Diagnostics[0];
+		var diagnosticSpan = diagnostic.Location.SourceSpan;
 
-		public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+		// Find the record class declaration identified by the diagnostic.
+		var recdeclaration = root?.FindToken(diagnosticSpan.Start).Parent?.AncestorsAndSelf().OfType<RecordDeclarationSyntax>().FirstOrDefault();
+		if (recdeclaration == null) return;
+
+		// build the fixer lambda, for record class or record struct
+		Func<CancellationToken, Task<Solution>> fixer = recdeclaration.Kind() switch
 		{
-			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+			SyntaxKind.RecordDeclaration => c => FixRecordClassAsync(context.Document, recdeclaration, c),
+			SyntaxKind.RecordStructDeclaration => c => FixRecordStructAsync(context.Document, recdeclaration, c),
+			_ => throw new NotImplementedException()
+		};
 
-			var diagnostic = context.Diagnostics[0];
-			var diagnosticSpan = diagnostic.Location.SourceSpan;
-
-			// Find the record class declaration identified by the diagnostic.
-			var recdeclaration = root?.FindToken(diagnosticSpan.Start).Parent?.AncestorsAndSelf().OfType<RecordDeclarationSyntax>().FirstOrDefault();
-			if (recdeclaration == null) return;
-
-			// build the fixer lambda, for record class or record struct
-			Func<CancellationToken, Task<Solution>> fixer = recdeclaration.Kind() switch
-			{
-				SyntaxKind.RecordDeclaration => c => FixRecordClassAsync(context.Document, recdeclaration, c),
-				SyntaxKind.RecordStructDeclaration => c => FixRecordStructAsync(context.Document, recdeclaration, c),
-				_ => throw new NotImplementedException()
-			};
-
-			// Register a code action for record class
-			context.RegisterCodeFix(
-				CodeAction.Create(
-					title: CodeFixResources.CodeFixTitle,
-					createChangedSolution: fixer,
-					equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
-				diagnostic);
-		}
+		// Register a code action for record class
+		context.RegisterCodeFix(
+			CodeAction.Create(
+				title: CodeFixResources.CodeFixTitle,
+				createChangedSolution: fixer,
+				equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
+			diagnostic);
+	}
 
 #pragma warning disable IDE0060 // Remove unused parameter
-		private async Task<Solution> FixRecordClassAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
-		{
-			await Task.CompletedTask.ConfigureAwait(false);
-			return document.Project.Solution;
 
-			/*
-			// Compute new uppercase name.
-			var identifierToken = typeDecl.Identifier;
-			var newName = identifierToken.Text.ToUpperInvariant();
+	/// <summary>
+	/// Code fix for record class
+	/// </summary>
+	private async Task<Solution> FixRecordClassAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+	{
+		/*	public virtual bool Equals(Self? other) => throw new NotSupported();
+			public override int GetHashCode() => 0;
+		 */
 
-			// Get the symbol representing the type to be renamed.
-			var semanticModel = await document.GetSemanticModelAsync(cancellationToken)
-				.ConfigureAwait(false);
-			var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+		await Task.CompletedTask.ConfigureAwait(false);
+		return document.Project.Solution;
 
-			// Produce a new solution that has all references to that type renamed, including the declaration.
-			//var originalSolution = document.Project.Solution;
-			//var optionSet = originalSolution.Workspace.Options; // this is obsolete
+		/*
+		// Compute new uppercase name.
+		var identifierToken = typeDecl.Identifier;
+		var newName = identifierToken.Text.ToUpperInvariant();
 
-			var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol,
-				  new SymbolRenameOptions(), newName, cancellationToken)
-				.ConfigureAwait(false);
+		// Get the symbol representing the type to be renamed.
+		var semanticModel = await document.GetSemanticModelAsync(cancellationToken)
+			.ConfigureAwait(false);
+		var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
 
-			// Return the new solution with the now-uppercase type name.
-			return newSolution;
-			*/
-		}
+		// Produce a new solution that has all references to that type renamed, including the declaration.
+		//var originalSolution = document.Project.Solution;
+		//var optionSet = originalSolution.Workspace.Options; // this is obsolete
 
-		private async Task<Solution> FixRecordStructAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
-		{
-			await Task.CompletedTask.ConfigureAwait(false);
-			return document.Project.Solution;
-		}
+		var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol,
+			  new SymbolRenameOptions(), newName, cancellationToken)
+			.ConfigureAwait(false);
+
+		// Return the new solution with the now-uppercase type name.
+		return newSolution;
+		*/
+	}
+
+	/// <summary>
+	/// Code fix for record struct
+	/// </summary>
+	private async Task<Solution> FixRecordStructAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+	{
+		/*	public readonly bool Equals(Self other) => throw new NotSupported();
+			public override int GetHashCode() => 0;
+		 */
+
+		await Task.CompletedTask.ConfigureAwait(false);
+		return document.Project.Solution;
 	}
 }
