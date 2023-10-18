b23eda0e1c65b273cec5aae29a56b65a5399d754
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2023-10-18 11:46:47 +0100

    Test project ignoring RCS1046, async methods ending with Async
    (cherry picked from commit 5cd62d205cebf7ff1d8a1d638be826051e07469c)

--- RecordValueAnalyser.Test/Program.cs
+++ RecordValueAnalyser.Test/Program.cs
@@ -2,8 +2,6 @@
 
 namespace RecordValueAnalyser.TestClasses;
 
-#pragma warning disable RCS1046 // Asynchronous method name should end with 'Async'.
-
 internal static class Program
 {
 	internal static async Task MainX()
